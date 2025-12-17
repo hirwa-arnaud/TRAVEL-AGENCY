bd4a02f897fce4a500409647dd614981ed4263cc
hirwa-arnaud <hirwatrevor8@gmail.com>
2025-12-17 14:42:45 +0200

    started

---
+++ my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar () {
+    return(
+        <>
+        <div className="flex justify-between items-center h-20">
+            <div>
+                <h1>TRAVELSY</h1>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Navbar
